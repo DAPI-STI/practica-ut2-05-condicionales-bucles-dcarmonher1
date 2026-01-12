2d507390c628a9bd27859df6d914724d2d5fcd51
David Carmona Herrera <dcarmonher1@educacion.navarra.es>
2026-01-12 12:10:09 +0100

    Ex04, 05 y 06 terminado

--- src/ex04_factorial.py
+++ src/ex04_factorial.py
@@ -12,4 +12,16 @@ def factorial(n: int) -> int:
     - Si n < 0, lanza ValueError.
     - Debe resolverse usando un bucle (no recursión).
     """
+    if n < 0:
+        raise ValueError("El número debe ser no negativo")
+    
+    if n == 0:
+        return 1
+    
+    resultado = 1
+    for i in range(1, n + 1):
+        resultado *= i
+        
+    return resultado
+
     raise NotImplementedError("Implementa factorial(n)")
--- src/ex05_table.py
+++ src/ex05_table.py
@@ -8,4 +8,10 @@ def multiplication_table(n: int) -> list[int]:
     Devuelve una lista con 10 elementos:
     [n*1, n*2, ..., n*10]
     """
+    tabla = []
+    for i in range(1, 11):
+        tabla.append(n * i)
+    
+    return tabla
+
     raise NotImplementedError("Implementa multiplication_table(n)")
